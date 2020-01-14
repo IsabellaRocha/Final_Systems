2141b9e25f9473d449f9fa79d5136efb4c98079d
IsabellaRocha <irocha00@stuy.edu>
2020-01-13 20:15:54 -0500

    Fixed creating a new file every time create new user is called

--- user.c
+++ user.c
@@ -19,37 +19,35 @@ int main() {
 }
 
 void display(char * choice) {
-  printf("hey");
-  char * newChoice = choice;
-  printf("%s", newChoice);
-  if(strcmp(newChoice, "log in") == 0) {
+  printf("%s", choice);
+  if(strcmp(choice, "log in") == 0) {
     system("clear"); //Clears output screen, from conio.h
     printf("Username: ");
   }
   if(verifyUser()) {
     system("clear");
-    printf("Please type in your choice from the options listed below: \n\n- View available cars (Select this if you also wish to rent a car)\n- View rented cars\n- View my account\n- Log out");
+    printf("Please type in your choice from the options listed below: \n\n- View available cars (Select this if you also wish to rent a car)\n- View rented cars\n- View my account\n- Log out\n\n");
   }
-  if(strcmp(newChoice, "create new account") == 0) {
+  if(strcmp(choice, "create new account") == 0) {
     system("clear");
-    printf("Username:");
+    printf("Username: ");
     if(makeUser()) {
       system("clear");
       printf("Password: ");
       if(makePassword()) {
         system("clear");
-        printf("Please type in your choice from the options listed below: \n\n- View available cars (Select this if you also wish to rent a car)\n- View rented cars\n- View my account\n- Log out");
+        printf("Please type in your choice from the options listed below: \n\n- View available cars (Select this if you also wish to rent a car)\n- View rented cars\n- View my account\n- Log out\n\n");
       }
     }
   }/*
-  if(strcmp(newChoice, "view available cars") == 0) {
+  if(strcmp(choice, "view available cars") == 0) {
     system("clear");
     int idx = 0;
     for(idx; availableCars[idx] != NULL; idx++) {
       printf("%s\n", availableCars[idx].model);
     }
   }
-  if(strcmp(newChoice, "view rented cars") == 0) {
+  if(strcmp(choice, "view rented cars") == 0) {
     system("clear");
     int idx = 0;
     rent();
@@ -57,10 +55,10 @@ void display(char * choice) {
       printf("%s\n", rentCars[idx].model);
     }
   }
-  if(strcmp(newChoice, "view my account") == 0) {
+  if(strcmp(choice, "view my account") == 0) {
     system("clear");
   }
-  if(strcmp(newChoice, "log out") == 0) {
+  if(strcmp(choice, "log out") == 0) {
     system("clear");
     logout();
     printf("Please choose whether you want to log in or create a new account:");
@@ -72,15 +70,21 @@ void rent();
 void logout();
 
 int makeUser() {
-  int fd = open("users.txt", O_CREAT|O_TRUNC, 0744);
-  if (fd < 0) {
-    printf("Error: %s", strerror(errno));
-    return 1;
+  if(fopen("users.txt", "r") == NULL) {
+    int fd = open("users.txt", O_CREAT|O_TRUNC, 0744);
+    if (fd < 0) {
+      printf("Error: %s", strerror(errno));
+      return 1;
+    }
+    close(fd);
   }
-  close(fd);
-  fd = open("users.txt", O_WRONLY|O_APPEND);
+  int fd = open("users.txt", O_WRONLY|O_APPEND);
   char input[SEG_SIZE];
   fgets(input, SEG_SIZE, stdin);
+  char * checker;
+  if ((checker = strchr(input, '\n')) != NULL) {
+    *checker = '\0';
+  }
   strncat(input, ",", 1);
   write(fd, input, strlen(input));
   close(fd);
@@ -91,7 +95,6 @@ int makePassword() {
   int fd = open("users.txt", O_WRONLY|O_APPEND);
   char input[SEG_SIZE];
   fgets(input, SEG_SIZE, stdin);
-  strncat(input, "\n", 1);
   write(fd, input, strlen(input));
   close(fd);
   return 1;
